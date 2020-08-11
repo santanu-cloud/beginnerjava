3514204fb6b4fdc017474f65ecd9924bc2e41fc7
pupu <netpupu@gmail.com>
2020-08-11 15:00:35 +0530

    my fist commit

---
+++ beginnerjava/src/beginnerjava/firstprogram.java
@@ -0,0 +1,15 @@
+
+package beginnerjava;
+
+public class firstprogram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hi This is our first program");
+		System.out.println("ready for some fun");
+
+	}
+
+}
